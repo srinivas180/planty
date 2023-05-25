758c2fadcb073de72d8c0f287a17708b556a0b5d
Srinivas <srinivas365@proton.me>
2023-05-25 10:33:05 +0530

    Added basic UI to Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+import "./Header.css"
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img className="logo__image" src="" width="50px" alt="" />
+            <h1 className="logo__name">Planty</h1>
+        </div>
+    );
+}
+
+function Search() {
+    return (
+        <div className="search">
+            <label className="search__label">
+                <input title="Search for products" className="search__input" type="search" placeholder="Search for products"/>
+            </label>
+        </div>
+    );
+}
+
+function Nav() {
+    return (
+        <nav className="nav">
+            <NavLink>Login</NavLink>
+            <NavLink>Wishlist</NavLink>
+            <NavLink>Cart</NavLink>
+        </nav>
+    );
+}
+
+export function Header() {
+    return (
+        <header className="header">
+            <Logo />
+            <Nav />
+            <Search />
+        </header>
+    )
+}
