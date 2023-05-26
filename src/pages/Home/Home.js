479c6d258e0a2725402543ef12931f1736d5914b
Srinivas <srinivas365@proton.me>
2023-05-26 11:01:54 +0530

    Added Home page with basic UI

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import { Category } from "../../components/Category/Category";
+import "./Home.css";
+
+export function Home() {
+    return (
+        <div className="container">
+            <h2>Categories</h2>
+            <div className="categories">
+                <ul className="categories__list">
+
+                </ul>
+            </div>
+        </div>
+    )
+}
