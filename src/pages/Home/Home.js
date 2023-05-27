d4d1e6abafba83f1ce9ca20aeb7c377bdfa203b2
Srinivas <srinivas365@proton.me>
2023-05-27 09:58:52 +0530

    Rendering categories in Home page using categories context

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,22 @@
-import { Category } from "../../components/Category/Category";
+import { useContext } from "react";
+
 import "./Home.css";
+import { CategoriesContext } from "../../contexts/CategoriesProvider";
+import { Category } from "../../components/Category/Category";
 
 export function Home() {
+    const { categories } = useContext(CategoriesContext);
+
     return (
         <div className="container">
             <h2>Categories</h2>
             <div className="categories">
                 <ul className="categories__list">
-
+                    {
+                        categories?.map(category => 
+                            <Category category={category} />
+                        )
+                    }
                 </ul>
             </div>
         </div>
