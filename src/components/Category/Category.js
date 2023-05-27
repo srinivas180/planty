e103027dc86ac4e06eac62c27ee9f5a20d063970
Srinivas <srinivas365@proton.me>
2023-05-27 10:01:20 +0530

    style: Added image styling to category component

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,7 +1,9 @@
+import "./Category.css"
+
 export function Category({category}) {
     return (
-        <li className="category__item" key={category._id}>
-            <img src={category.imageLink} alt={category.altText} />
+        <li className="category__item" key={category.id}>
+            <img className="category__image" src={category.imageLink} alt={category.altText} />
             <h3>{category.categoryName}</h3>
             <p>{category.description}</p>
         </li>
