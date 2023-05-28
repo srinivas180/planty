f7df6edb12e26be675d3bf159e748a558b4102af
Srinivas <srinivas365@proton.me>
2023-05-28 19:46:23 +0530

    style: Added responsive styling to products page

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -3,13 +3,15 @@ import { useContext } from "react"
 import { ProductsContext } from "../../contexts/ProductsProvider";
 import { Product } from "../../components/Product/Product";
 
+import "./Products.css"
+
 export function Products() {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div className="container">
+        <div className="products">
             <h2>Products</h2>
-            <div className="products">
+            <div className="products__list">
                 {
                     products.map(product => (
                         <Product key={product.id} product={product} />
