9787c3b54bc84e770f2a100569b6b68320b49b52
Srinivas <srinivas365@proton.me>
2023-05-28 19:44:02 +0530

    Changed css class names of product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import "./Product.css";
 
 export function Product({ product }) {
     return (
-        <div className="product">
+        <div className="product__item">
             <img className="product__image" src={product.imageLink} />
             <h3 className="prdudct__heading">{product.title}</h3>
         </div>
