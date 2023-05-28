4cec583b7f195c139ef7950ea443a042b5ad1934
Srinivas <srinivas365@proton.me>
2023-05-28 11:33:56 +0530

    style: Added image border and styling for category heading, description

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -4,8 +4,8 @@ export function Category({category}) {
     return (
         <li className="category__item" key={category.id}>
             <img className="category__image" src={category.imageLink} alt={category.altText} />
-            <h3>{category.categoryName}</h3>
-            <p>{category.description}</p>
+            <h3 className="category__heading">{category.categoryName}</h3>
+            <p className="category__description">{category.description}</p>
         </li>
     )
 }
