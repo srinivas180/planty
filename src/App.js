3fcc93fc9eef9f126e8b56650761ce996e1a83c6
Srinivas <srinivas365@proton.me>
2023-05-28 12:05:39 +0530

    Added Home and Products routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import "./App.css";
+import {Routes, Route} from "react-router-dom";
 
+import "./App.css";
 import { Header } from "./components/Header/Header";
-import { Home } from "./pages/Home/Home"
+import { Home } from "./pages/Home/Home";
+import { Products } from "./pages/Products/Products"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
