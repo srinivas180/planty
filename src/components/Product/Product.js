3d595cf0071acab706bf183e63d6b8c9c1b01e6d
Srinivas <srinivas365@proton.me>
2023-05-30 12:59:06 +0530

    Added, add to cart and wishlist buttons

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,6 +5,10 @@ export function Product({ product }) {
         <div className="product__item">
             <img className="product__image" src={product.imageLink} />
             <h3 className="prdudct__heading">{product.title}</h3>
+            <div className="product__buttons">
+                <button className="product__button product__button--secondary">Add to wishlist</button>
+                <button className="product__button product__button--primary">Add to cart</button>
+            </div>
         </div>
     )
 }
