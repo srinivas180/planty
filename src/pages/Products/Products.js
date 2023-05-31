11304ab7efd87d21624bdaa8fb61e0e1a66bc636
Srinivas <srinivas365@proton.me>
2023-05-31 10:54:53 +0530

    Added category filters UI

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -9,14 +9,44 @@ export function Products() {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div className="products">
-            <h2>Products</h2>
-            <div className="products__list">
-                {
-                    products.map(product => (
-                        <Product key={product.id} product={product} />
-                    ))
-                }
+        <div className="row container">
+            <div class="filters">
+                <h2>Filters</h2>
+
+                <div className="filters__category">
+                    <h3 className="filters__heading">Category</h3>
+                    <label>
+                        <input type="checkbox"/>
+                        Indoor Plants
+                    </label>
+                    <label>
+                        <input type="checkbox"/>
+                        Outdoor Plants
+                    </label>
+                    <label>
+                        <input type="checkbox"/>
+                        Air Purifier Plants
+                    </label>
+                    <label>
+                        <input type="checkbox"/>
+                        Mosquito Repellent Plants
+                    </label>
+                    <label>
+                        <input type="checkbox"/>
+                        Bonsai Plants
+                    </label>
+                </div>
+
+            </div>
+            <div className="products">
+                <h2>Products</h2>
+                <div className="products__list">
+                    {
+                        products.map(product => (
+                            <Product key={product.id} product={product} />
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
