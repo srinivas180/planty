50eb0e221a2e311c8f5423b36ac8a7a9e6d2f9d7
Srinivas <srinivas365@proton.me>
2023-05-31 15:38:37 +0530

    Added cart route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Routes, Route} from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
-import { Products } from "./pages/Products/Products"
+import { Products } from "./pages/Products/Products";
+import { Cart } from "./pages/Cart/Cart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
