032750291cf4e59a8e6732332ce6aee9d9a4da6a
Srinivas <srinivas365@proton.me>
2023-06-01 18:34:09 +0530

    Added wishlist route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Header } from "./components/Header/Header";
 import { Home } from "./pages/Home/Home";
 import { Products } from "./pages/Products/Products";
 import { Cart } from "./pages/Cart/Cart";
+import { Wishlist } from "./pages/Wishlist/Wishlist"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
     </div>
   );
