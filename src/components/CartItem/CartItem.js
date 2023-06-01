4741fa5301cfccdcc1a92415ab0b2c5a060b4528
Srinivas <srinivas365@proton.me>
2023-06-01 10:42:55 +0530

    Added quantity div and class names

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,12 +1,26 @@
+import "./CartItem.css";
+
 export function CartItem({ item }) {
     return (
         <div className="cart-item">
             <img className="cart-item__image" src={item.imageLink} alt={item.altText}/>
-            <h2 className="cart-item__heading">{item.title}</h2>
-            <p>Category: {item.categoryName}</p>
-            <button>Remove Item</button>
-            <button>Move to Wishlist</button>
-            <p>₹{item.price}</p>
+            <div className="cart-item__details">
+                <h2 className="cart-item__heading">{item.title}</h2>
+                <p className="cart-item__price">₹{item.price}</p>
+                <div className="cart-item__category">
+                    <span className="category__title">Category: </span> {item.categoryName}
+                </div>
+                <div className="quantity">
+                    <span className="quantity__title">Quantity: </span>
+                    <button className="quantity__button button--secondary">-</button>
+                    <span className="quantity__number">1</span>
+                    <button className="quantity__button button--secondary">+</button>
+                </div>
+                <div className="cart-item__buttons">
+                    <button className="cart-item__button button--secondary">Remove Item</button>
+                    <button className="cart-item__button button--secondary">Move to Wishlist</button>
+                </div>
+            </div>
         </div>
     )
 }
