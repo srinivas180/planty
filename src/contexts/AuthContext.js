e23d5b45c214e68ef6d8c744b7776b32a43a0290
Srinivas <srinivas365@proton.me>
2023-06-02 21:58:29 +0530

    feature: Added login handler in auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
+    const [encodedToken, setEncodedToken] = useState();
+    const [user, setUser] = useState();
+
+    async function loginHandler(userCredentials) {
+        const response = await fetch("/api/auth/login", {
+            method: 'POST',
+            body: JSON.stringify(userCredentials),
+        });
+    
+        const { encodedToken, user } = await response.json();
+        
+        localStorage.setItem("encodedToken", encodedToken);
+        localStorage.setItem("user", user);
+        
+        setEncodedToken(encodedToken);
+        setUser(user);
+    }
+
     return (
-        <AuthContext.Provider value={{ encodedToken }} >
+        <AuthContext.Provider value={{ encodedToken, user, loginHandler }} >
             { children }
         </AuthContext.Provider>
     );
