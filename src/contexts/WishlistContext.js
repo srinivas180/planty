23dbcb40b2089961b3b20f91ef0cbe84aea27aa4
Srinivas <srinivas365@proton.me>
2023-06-03 11:35:36 +0530

    Replaced useReducer with useState

--- src/contexts/WishlistContext.js
+++ src/contexts/WishlistContext.js
@@ -1,23 +1,12 @@
-import { createContext, useReducer } from "react";
+import { createContext, useState } from "react";
 
 export const WishlistContext = createContext();
 
-function WishlistReducer(wishlist, action) {
-    switch(action.type) {
-        case "ADD_TO_WISHLIST":
-            break;
-        case "REMOVE_FROM_WISHLIST":
-            break;
-        default:
-            return wishlist;
-    }
-}
-
 export function WishlistProvider({ children }) {
-    const [wishlist, dispatchWishlist] = useReducer(WishlistReducer, []);
+    const [wishlist, setWishlist] = useState([]);
 
     return (
-        <WishlistContext.Provider value={{ wishlist, dispatchWishlist }}>
+        <WishlistContext.Provider value={{ wishlist }}>
             { children }
         </WishlistContext.Provider>
     );
