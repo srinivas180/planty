b054147159152365b8e307eb39b5faf2bff1c7e7
Srinivas <srinivas365@proton.me>
2023-06-03 16:12:32 +0530

    typo: product heading

--- src/components/WishlistItem/WishlistItem.js
+++ src/components/WishlistItem/WishlistItem.js
@@ -10,7 +10,7 @@ export function WishlistItem({ product }) {
     return (
         <div className="product__item">
             <img className="product__image" src={product.imageLink} />
-            <h3 className="prdudct__heading">{product.title}</h3>
+            <h3 className="product__heading">{product.title}</h3>
             <div className="product__buttons">
                 <button className="product__button product__button--secondary" 
                     onClick={() => removeFromWishlist(product._id)}
