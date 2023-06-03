04b3ead42dba314c5be49ddf223f0b218075e5db
Srinivas <srinivas365@proton.me>
2023-06-03 11:42:17 +0530

    feature: Added add to wishlist functionality

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
+import { WishlistContext } from "../../contexts/WishlistContext";
 
 import "./Product.css";
 
 export function Product({ product }) {
     const { addToCart } = useContext(CartContext);
+    const { addToWishlist } = useContext(WishlistContext);
 
     return (
         <div className="product__item">
             <img className="product__image" src={product.imageLink} />
             <h3 className="prdudct__heading">{product.title}</h3>
             <div className="product__buttons">
-                <button className="product__button product__button--secondary">Add to wishlist</button>
+                <button className="product__button product__button--secondary" onClick={() => addToWishlist(product)}>Add to wishlist</button>
                 <button className="product__button product__button--primary" onClick={() => addToCart(product)}>Add to cart</button>
             </div>
         </div>
--- src/contexts/WishlistContext.js
+++ src/contexts/WishlistContext.js
@@ -1,12 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
+
+import { AuthContext } from "./AuthContext";
 
 export const WishlistContext = createContext();
 
 export function WishlistProvider({ children }) {
     const [wishlist, setWishlist] = useState([]);
+    const { encodedToken } = useContext(AuthContext);
+
+    async function addToWishlist(product) {
+        const response = await fetch("api/user/wishlist", {
+            method: "POST",
+            headers: {
+                "authorization": encodedToken,
+            },
+            body: JSON.stringify({
+                product
+            }),
+        });
+
+        const data = await response.json();
+        setWishlist(data.wishlist);
+    }
 
     return (
-        <WishlistContext.Provider value={{ wishlist }}>
+        <WishlistContext.Provider value={{ wishlist, addToWishlist }}>
             { children }
         </WishlistContext.Provider>
     );
