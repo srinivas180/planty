0b86942d18c09dc1084fe6c5cded153072a7483c
Srinivas <srinivas365@proton.me>
2023-06-04 12:22:40 +0530

    Refactor: moved filters components to main products component

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -5,61 +5,61 @@ import { Product } from "../../components/Product/Product";
 
 import "./Products.css"
 
-function CategoryFilter() {
-    return (
-        <div className="filters__container">
-            <h3 className="filters__heading">Category</h3>
-            <label className="filters__label">
-                <input className="filters__input" type="checkbox"/>
-                Indoor Plants
-            </label>
-            <label className="filters__label">
-                <input className="filters__input" type="checkbox"/>
-                Outdoor Plants
-            </label>
-            <label className="filters__label">
-                <input className="filters__input" type="checkbox"/>
-                Air Purifier Plants
-            </label>
-            <label className="filters__label">
-                <input className="filters__input" type="checkbox"/>
-                Mosquito Repellent Plants
-            </label>
-            <label className="filters__label">
-                <input className="filters__input" type="checkbox"/>
-                Bonsai Plants
-            </label>
-        </div>
-    );
-}
-
-function RatingFilter() {
-    return (
-        <div className="filters__container">
-            <h3 className="filters__heading">Rating</h3>
-            <input className="filters__input input__range" type="range" min="1" max="5" step="1" defaultValue="3"/>
-        </div>
-    );
-}
-
-function PriceSortFilter() {
-    return (
-        <div className="filters__container">
-            <h3 className="filters__heading">Sort By</h3>
-            <label className="filters__label">
-                <input className="filters__input" type="radio" name="rating"/>
-                Price - Low to High
-            </label>
-            <label className="filters__label">
-                <input className="filters__input" type="radio" name="rating"/>
-                Price - High to Low
-            </label>
-        </div>
-    );
-}
-
 export function Products() {
     const { products } = useContext(ProductsContext);
+
+    function CategoryFilter() {
+        return (
+            <div className="filters__container">
+                <h3 className="filters__heading">Category</h3>
+                <label className="filters__label">
+                    <input className="filters__input" type="checkbox"/>
+                    Indoor Plants
+                </label>
+                <label className="filters__label">
+                    <input className="filters__input" type="checkbox"/>
+                    Outdoor Plants
+                </label>
+                <label className="filters__label">
+                    <input className="filters__input" type="checkbox"/>
+                    Air Purifier Plants
+                </label>
+                <label className="filters__label">
+                    <input className="filters__input" type="checkbox"/>
+                    Mosquito Repellent Plants
+                </label>
+                <label className="filters__label">
+                    <input className="filters__input" type="checkbox"/>
+                    Bonsai Plants
+                </label>
+            </div>
+        );
+    }
+    
+    function RatingFilter() {
+        return (
+            <div className="filters__container">
+                <h3 className="filters__heading">Rating</h3>
+                <input className="filters__input input__range" type="range" min="1" max="5" step="1" defaultValue="3"/>
+            </div>
+        );
+    }
+    
+    function PriceSortFilter() {
+        return (
+            <div className="filters__container">
+                <h3 className="filters__heading">Sort By</h3>
+                <label className="filters__label">
+                    <input className="filters__input" type="radio" name="rating"/>
+                    Price - Low to High
+                </label>
+                <label className="filters__label">
+                    <input className="filters__input" type="radio" name="rating"/>
+                    Price - High to Low
+                </label>
+            </div>
+        );
+    }
 
     return (
         <div className="row container">
