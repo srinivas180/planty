5bb46dfb5e7f09550c9f7cd8c57557ff90809322
Srinivas <srinivas365@proton.me>
2023-06-05 18:44:06 +0530

    feature: Added search functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,41 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
 
+import { ProductsContext } from "../../contexts/ProductsContext";
 import "./Header.css"
 
 export function Header() {
+    const { setFilters } = useContext(ProductsContext);
 
-    function Logo() {
-        return (
+    return (
+        <header className="header">
             <div className="logo">
                 <img className="logo__image" src="" width="50px" alt="" />
                 <h1 className="logo__name">Planty</h1>
             </div>
-        );
-    }
-    
-    function Search() {
-        return (
-            <div className="search">
-                <label className="search__label">
-                    <input title="Search for products" className="search__input" type="search" placeholder="Search for products"/>
-                </label>
-            </div>
-        );
-    }
-    
-    function Nav() {
-        return (
             <nav className="nav">
                 <NavLink className="nav__link" to="/login">Login</NavLink>
                 <NavLink className="nav__link" to="/products">Products</NavLink>
                 <NavLink className="nav__link" to="/wishlist">Wishlist</NavLink>
                 <NavLink className="nav__link" to="/cart">Cart</NavLink>
             </nav>
-        );
-    }
-    
-    return (
-        <header className="header">
-            <Logo />
-            <Nav />
-            <Search />
+
+            <div className="search">
+                <label className="search__label">
+                    <input
+                        title="Search for products"
+                        className="search__input"
+                        type="search"
+                        placeholder="Search for products"
+                        onChange={(event) => setFilters(filters => (
+                            {
+                                ...filters,
+                                searchQuery: event.target.value,
+                            }
+                        ))}
+                    />
+                </label>
+            </div>
         </header>
     )
 }
