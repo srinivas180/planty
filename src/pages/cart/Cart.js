d0f119fa11dd5f78461b3bee0a3d380b61d9035e
Srinivas <srinivas365@proton.me>
2023-06-07 11:41:27 +0530

    extracted price details to individual component

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -2,46 +2,12 @@ import { useContext } from "react";
 
 import { CartContext } from "../../contexts/CartContext";
 import { CartItem } from "../../components/CartItem/CartItem";
+import { PriceDetails } from "../../components/PriceDetails/PriceDetails";
 
 import "./Cart.css";
 
 export function Cart() {
-    const {cart, getItemsPrice} = useContext(CartContext);
-    const itemsPrice = getItemsPrice();
-
-    function PriceDetails() {
-        return (
-            <div className="price-details">
-                <h2 className="price__heading">Price Details</h2>
-
-                <div className="price__split">
-                    <div className="price__item">
-                        <p className="price__attribute">Price ({ cart.length } items)</p>
-                        <p className="price__value">₹{itemsPrice}.00</p>
-                    </div>
-                    <div className="price__item">
-                        <p className="price__attribute">Discount</p>
-                        <p className="price__value">-₹100.00</p>
-                    </div>
-                    <div className="price__item">
-                        <p className="price__attribute">Delivery Charges</p>
-                        <p className="price__value">₹100.00</p>
-                    </div>
-                </div>
-
-                <div className="total-price price__item">
-                    <p className="total-price__attribute">Total Amount</p>
-                    <p className="total-price__value">₹{itemsPrice}.00</p>
-                </div>
-
-                <p className="price__savings">
-                    You will save ₹100.00 on this order
-                </p>
-
-                <button className="button--primary price__checkout">Checkout</button>
-            </div>
-        )
-    }
+    const {cart} = useContext(CartContext);
 
     return (
         <div className="container row row--center">
