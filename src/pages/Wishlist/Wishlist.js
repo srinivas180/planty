b9aa006d3b91a3027146e1548236933ffef1f267
Srinivas <srinivas365@proton.me>
2023-06-07 15:30:23 +0530

    show text and link to products if cart is empty

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,4 +1,5 @@
-import { useContext } from "react"
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
 import { WishlistContext } from "../../contexts/WishlistContext";
 import { WishlistItem } from "../../components/WishlistItem/WishlistItem";
@@ -7,15 +8,26 @@ export function Wishlist() {
     const { wishlist } = useContext(WishlistContext);
 
     return (
-        <div className="products container">
-        <h2>Wishlist ({ wishlist.length } items)</h2>
-        <div className="products__list">
+        <>
             {
-                wishlist.map(product => (
-                    <WishlistItem key={product.id} product={product} />
-                ))
+                wishlist.length === 0 ? (
+                    <div className="container cart--empty">
+                        <h2>Your wishlist is empty</h2>
+                        <NavLink className="explore-button" to="/products">Explore products</NavLink>
+                    </div>
+                ) : (
+                    <div className="products container">
+                        <h2>Wishlist ({ wishlist.length } items)</h2>
+                        <div className="products__list">
+                            {
+                                wishlist.map(product => (
+                                    <WishlistItem key={product.id} product={product} />
+                                ))
+                            }
+                        </div>
+                    </div>
+                )
             }
-        </div>
-    </div>
+        </>
     )
 }
