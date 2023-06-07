f4cb4c5105256db3a28244f0e0c4907728d90c7f
Srinivas <srinivas365@proton.me>
2023-06-07 15:25:54 +0530

    shows text and link to products if cart is empty

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -4,51 +4,63 @@ import { CartContext } from "../../contexts/CartContext";
 import { CartItem } from "../../components/CartItem/CartItem";
 
 import "./Cart.css";
+import { NavLink } from "react-router-dom";
 
 export function Cart() {
     const {cart, getItemsPrice} = useContext(CartContext);
     const itemsPrice = getItemsPrice();
 
     return (
-        <div className="container row row--center">
-            <div className="cart">
-                <h2>Cart ({ cart.length } items)</h2>
-                <div className="cart__items">
-                {
-                    cart.map(item => <CartItem key={item.id} item={item} />)
-                }
-                </div>
-            </div>
-
-            <div className="price-details">
-                <h2 className="price__heading">Price Details</h2>
-
-                <div className="price__split">
-                    <div className="price__item">
-                        <p className="price__attribute">Price ({ cart.length } items)</p>
-                        <p className="price__value">₹{itemsPrice}.00</p>
+        <>
+            {
+                cart.length === 0 ? (
+                    <div className="container cart--empty">
+                        <h2>Your cart is empty</h2>
+                        <NavLink className="explore-button" to="/products">Explore products</NavLink>
                     </div>
-                    <div className="price__item">
-                        <p className="price__attribute">Discount</p>
-                        <p className="price__value">-₹100.00</p>
+                ) : (
+                    <div className="container row row--center">
+                        <div className="cart">
+                            <h2>Cart ({ cart.length } items)</h2>
+                            <div className="cart__items">
+                            {
+                                cart.map(item => <CartItem key={item.id} item={item} />)
+                            }
+                            </div>
+                        </div>
+            
+                        <div className="price-details">
+                            <h2 className="price__heading">Price Details</h2>
+            
+                            <div className="price__split">
+                                <div className="price__item">
+                                    <p className="price__attribute">Price ({ cart.length } items)</p>
+                                    <p className="price__value">₹{itemsPrice}.00</p>
+                                </div>
+                                <div className="price__item">
+                                    <p className="price__attribute">Discount</p>
+                                    <p className="price__value">-₹100.00</p>
+                                </div>
+                                <div className="price__item">
+                                    <p className="price__attribute">Delivery Charges</p>
+                                    <p className="price__value">₹100.00</p>
+                                </div>
+                            </div>
+            
+                            <div className="total-price price__item">
+                                <p className="total-price__attribute">Total Amount</p>
+                                <p className="total-price__value">₹{itemsPrice}.00</p>
+                            </div>
+            
+                            <p className="price__savings">
+                                You will save ₹100.00 on this order
+                            </p>
+            
+                            <button className="button--primary price__checkout">Checkout</button>
+                        </div>
                     </div>
-                    <div className="price__item">
-                        <p className="price__attribute">Delivery Charges</p>
-                        <p className="price__value">₹100.00</p>
-                    </div>
-                </div>
-
-                <div className="total-price price__item">
-                    <p className="total-price__attribute">Total Amount</p>
-                    <p className="total-price__value">₹{itemsPrice}.00</p>
-                </div>
-
-                <p className="price__savings">
-                    You will save ₹100.00 on this order
-                </p>
-
-                <button className="button--primary price__checkout">Checkout</button>
-            </div>
-        </div>
+                )
+            }
+        </>
     )
 }
