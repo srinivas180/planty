f0369f420f333a82dc2522f41aef7fd58571c9d2
Srinivas <srinivas365@proton.me>
2023-07-22 10:59:09 +0530

    redesigned categories

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,31 +1,36 @@
 import { useContext } from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 import { ProductsContext } from "../../contexts/ProductsContext";
 
-import "./Category.css"
+import "./Category.css";
 
-export function Category({category, index}) {
+export function Category({ category, index }) {
     const { setFilters } = useContext(ProductsContext);
     const navigate = useNavigate();
 
     return (
-        <li className="category__item"
+        <li
+            className="category__item"
             onClick={() => {
-                setFilters(
-                    filters => (
-                        {
-                            ...filters,
-                            categoriesCheckedState: filters.categoriesCheckedState.map((state, position) => index == position ? true : false),
-                        }
-                    )
-                );
-                navigate("/products")
+                setFilters((filters) => ({
+                    ...filters,
+                    categoriesCheckedState: filters.categoriesCheckedState.map(
+                        (state, position) => (index == position ? true : false)
+                    ),
+                }));
+                navigate("/products");
             }}
         >
-            <img className="category__image" src={category.imageLink} alt={category.altText} />
+            <div className="category__image-wrapper">
+                <img
+                    className="category__image"
+                    src={category.imageLink}
+                    alt={category.altText}
+                />
+            </div>
             <h3 className="category__heading">{category.categoryName}</h3>
             <p className="category__description">{category.description}</p>
         </li>
-    )
+    );
 }
