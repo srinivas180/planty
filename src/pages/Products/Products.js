5cc21d799037311e3c8e0b52a396518ce000f7bd
Srinivas <srinivas365@proton.me>
2023-07-22 18:11:02 +0530

    refactor: clear filters button

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,15 +1,16 @@
-import { useContext } from "react"
+import { useContext } from "react";
 
 import { Loader } from "../../components/Spinner/Spinner";
 import { ProductsContext } from "../../contexts/ProductsContext";
 import { Product } from "../../components/Product/Product";
 import { ProductsFilter } from "../../components/ProductsFilter/ProductsFilter";
 
-import "./Products.css"
+import "./Products.css";
 import { CategoriesContext } from "../../contexts/CategoriesContext";
 
 export function Products() {
-    const { filters, setFilters, products, filteredProducts } = useContext(ProductsContext);
+    const { filters, setFilters, products, filteredProducts } =
+        useContext(ProductsContext);
     const { categories } = useContext(CategoriesContext);
 
     return (
@@ -18,32 +19,39 @@ export function Products() {
                 <div className="filters__head">
                     <h2 className="filters__title">Filters</h2>
                     <button
-                        className="button--primary"
-                        onClick={() => setFilters({
-                            searchQuery: filters.searchQuery,
-                            categoriesCheckedState: categories.map(() => false),
-                            rating: 1,
-                            sortLowToHigh: false,
-                            sortHighToLow: false,
-                        })}
-                    >Clear Filters</button>
+                        className="button button--primary"
+                        onClick={() =>
+                            setFilters({
+                                searchQuery: filters.searchQuery,
+                                categoriesCheckedState: categories.map(
+                                    () => false
+                                ),
+                                rating: 1,
+                                sortLowToHigh: false,
+                                sortHighToLow: false,
+                            })
+                        }
+                    >
+                        Clear Filters
+                    </button>
                 </div>
                 <ProductsFilter />
             </div>
             <div className="products">
-                <h2>Products (showing { filteredProducts.length } of { products.length } plants)</h2>
+                <h2>
+                    Products (showing {filteredProducts.length} of{" "}
+                    {products.length} plants)
+                </h2>
                 <div className="products__list">
-                    {
-                        filteredProducts.length === 0 ? (
-                            <Loader/>
-                        ) : (
-                            filteredProducts.map(product => (
-                                <Product key={product.id} product={product} />
-                            ))
-                        )
-                    }
+                    {filteredProducts.length === 0 ? (
+                        <Loader />
+                    ) : (
+                        filteredProducts.map((product) => (
+                            <Product key={product.id} product={product} />
+                        ))
+                    )}
                 </div>
             </div>
         </div>
-    )
+    );
 }
