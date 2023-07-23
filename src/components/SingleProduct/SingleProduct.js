737ec62f3fdb311ee18ce4d93ffb6d5866b169f1
Srinivas <srinivas365@proton.me>
2023-07-23 12:00:38 +0530

    show loading spinner in single product viewing page

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -9,64 +9,84 @@ import { Loader } from "../../components/Spinner/Spinner";
 import "./SingleProduct.css";
 
 export function SingleProduct() {
-
     const { productId } = useParams();
     const [product, setProduct] = useState();
 
-    const { addToCart, cartHasProduct, navigateToCart } = useContext(CartContext);
-    const { addToWishlist, removeFromWishlist, wishlistHasItem } = useContext(WishlistContext);
+    const { addToCart, cartHasProduct, navigateToCart } =
+        useContext(CartContext);
+    const { addToWishlist, removeFromWishlist, wishlistHasItem } =
+        useContext(WishlistContext);
+
+    const [isLoading, setIsLoading] = useState(true);
 
     async function fetchProduct() {
-        const response = await fetch((`/api/products/${productId}`));
+        const response = await fetch(`/api/products/${productId}`);
         const { product } = await response.json();
         setProduct(product);
+        setIsLoading(false);
     }
 
     useEffect(() => {
         fetchProduct();
     }, []);
 
-
     return (
         <>
-            { 
-                (product == undefined) ? <Loader/> : (
-                    <div className="cart-item single-product">
-                        <img className="cart-item__image" src={product.imageLink} alt={product.altText}/>
-                        <div className="cart-item__details single-product__details">
-                            <h2 className="cart-item__heading">{product.title}</h2>
-                            <p className="cart-item__price">₹{product.price}</p>
-                            <div className="cart-item__category">
-                                <span className="category__title">Category: </span> {product.categoryName}
-                            </div>
-                            <div className="single-product__description">
-                                Lorem magna euismod magna ipsum et sit dolore ea eleifend voluptua labore kasd lorem enim ipsum ipsum amet.
-                                Lorem vero ipsum sea ea duo ea stet justo ut no ut. Sed euismod laoreet consectetuer volutpat amet adipiscing
-                                accusam tincidunt dolore diam et et te amet nulla.
-                            </div>
-                            <div className="cart-item__buttons">
-                                <button className="cart-item__button button--secondary" onClick={() => {
-                                    cartHasProduct(product) ? navigateToCart() : addToCart(product);
-                                }}>
-                                    {
-                                        cartHasProduct(product ?? false) ? "Go to cart" : "Add to cart"
-                                    }
-                                </button>
-                                <button 
-                                    className="cart-item__button button--secondary"
-                                    onClick={() => {
-                                        wishlistHasItem(product) ? removeFromWishlist(product._id) : addToWishlist(product)
-                                    }}
-                                >
-                                    {
-                                        wishlistHasItem(product ?? false) ? "Unwishlist" : "Add to wishlist"
-                                    }
-                                </button>
-                            </div>
+            {isLoading ? (
+                <div className="loader">
+                    <Loader />
+                </div>
+            ) : (
+                <div className="cart-item single-product">
+                    <img
+                        className="cart-item__image"
+                        src={product.imageLink}
+                        alt={product.altText}
+                    />
+                    <div className="cart-item__details single-product__details">
+                        <h2 className="cart-item__heading">{product.title}</h2>
+                        <p className="cart-item__price">₹{product.price}</p>
+                        <div className="cart-item__category">
+                            <span className="category__title">Category: </span>{" "}
+                            {product.categoryName}
+                        </div>
+                        <div className="single-product__description">
+                            Lorem magna euismod magna ipsum et sit dolore ea
+                            eleifend voluptua labore kasd lorem enim ipsum ipsum
+                            amet. Lorem vero ipsum sea ea duo ea stet justo ut
+                            no ut. Sed euismod laoreet consectetuer volutpat
+                            amet adipiscing accusam tincidunt dolore diam et et
+                            te amet nulla.
+                        </div>
+                        <div className="cart-item__buttons">
+                            <button
+                                className="cart-item__button button--secondary"
+                                onClick={() => {
+                                    cartHasProduct(product)
+                                        ? navigateToCart()
+                                        : addToCart(product);
+                                }}
+                            >
+                                {cartHasProduct(product ?? false)
+                                    ? "Go to cart"
+                                    : "Add to cart"}
+                            </button>
+                            <button
+                                className="cart-item__button button--secondary"
+                                onClick={() => {
+                                    wishlistHasItem(product)
+                                        ? removeFromWishlist(product._id)
+                                        : addToWishlist(product);
+                                }}
+                            >
+                                {wishlistHasItem(product ?? false)
+                                    ? "Unwishlist"
+                                    : "Add to wishlist"}
+                            </button>
                         </div>
                     </div>
-                )
-            }
+                </div>
+            )}
         </>
     );
 }
