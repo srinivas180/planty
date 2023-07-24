0d4c663b99362bbeb35470eb890dd284757c16ad
Srinivas <srinivas365@proton.me>
2023-07-24 16:51:22 +0530

    added button class to login as guest button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -15,32 +15,47 @@ export function Login() {
     return (
         <div className="container column column--center">
             <form className="form column">
-            <h2 className="form__heading">Login</h2>
+                <h2 className="form__heading">Login</h2>
 
-                <input className="form__input" type="email" placeholder="Email" />
-                <input className="form__input" type="password" placeholder="Password"/>
+                <input
+                    className="form__input"
+                    type="email"
+                    placeholder="Email"
+                />
+                <input
+                    className="form__input"
+                    type="password"
+                    placeholder="Password"
+                />
                 <div className="row row--space-between">
                     <label>
-                        <input type="checkbox"/>
+                        <input type="checkbox" />
                         Remember me
                     </label>
-                    <NavLink className="form__navlink navlink--no-underline">Forgot password</NavLink>
+                    <NavLink className="form__navlink navlink--no-underline">
+                        Forgot password
+                    </NavLink>
                 </div>
-                <button className="button--primary form__submit" type="submit">Login</button>
-                <button className="button--text form__guest-login-button" 
-                    onClick={
-                        (event) => {
-                            event.preventDefault();
-                            loginHandler(guestUserCredentials)
-                        }
-                    }
-                >Login as Guest</button>
+                <button className="button--primary form__submit" type="submit">
+                    Login
+                </button>
+                <button
+                    className="button button--text form__guest-login-button"
+                    onClick={(event) => {
+                        event.preventDefault();
+                        loginHandler(guestUserCredentials);
+                    }}
+                >
+                    Login as Guest
+                </button>
             </form>
             <div>
                 Don't have account?
-                <NavLink className="form__navlink" to="/signup"> Signup</NavLink>
+                <NavLink className="form__navlink" to="/signup">
+                    {" "}
+                    Signup
+                </NavLink>
             </div>
-            
         </div>
-    )
+    );
 }
