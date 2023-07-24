0615969183d8cc33e36ab981a16ca1c61a65a18a
Srinivas <srinivas365@proton.me>
2023-07-24 18:16:37 +0530

    added button class wherever required

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -6,37 +6,61 @@ import "./CartItem.css";
 
 export function CartItem({ item }) {
     const { removeFromCart, quantityHandler } = useContext(CartContext);
-    const { addToWishlist, removeFromWishlist, wishlistHasItem } = useContext(WishlistContext);
+    const { addToWishlist, removeFromWishlist, wishlistHasItem } =
+        useContext(WishlistContext);
 
     return (
         <div className="cart-item">
-            <img className="cart-item__image" src={item.imageLink} alt={item.altText}/>
+            <img
+                className="cart-item__image"
+                src={item.imageLink}
+                alt={item.altText}
+            />
             <div className="cart-item__details">
                 <h2 className="cart-item__heading">{item.title}</h2>
                 <p className="cart-item__price">₹{item.price}</p>
                 <div className="cart-item__category">
-                    <span className="category__title">Category: </span> {item.categoryName}
+                    <span className="category__title">Category: </span>{" "}
+                    {item.categoryName}
                 </div>
                 <div className="quantity">
                     <span className="quantity__title">Quantity: </span>
-                    <button className="quantity__button button--secondary" onClick={() => quantityHandler(item._id, "decrement")} disabled={item.qty === 1}>-</button>
+                    <button
+                        className="quantity__button button  button--secondary"
+                        onClick={() => quantityHandler(item._id, "decrement")}
+                        disabled={item.qty === 1}
+                    >
+                        -
+                    </button>
                     <span className="quantity__number">{item.qty}</span>
-                    <button className="quantity__button button--secondary" onClick={() => quantityHandler(item._id, "increment")}>+</button>
+                    <button
+                        className="quantity__button button  button--secondary"
+                        onClick={() => quantityHandler(item._id, "increment")}
+                    >
+                        +
+                    </button>
                 </div>
                 <div className="cart-item__buttons">
-                    <button className="cart-item__button button--secondary" onClick={() => removeFromCart(item._id)}>Remove Item</button>
-                    <button 
-                        className="cart-item__button button--secondary"
+                    <button
+                        className="cart-item__button button  button--secondary"
+                        onClick={() => removeFromCart(item._id)}
+                    >
+                        Remove Item
+                    </button>
+                    <button
+                        className="cart-item__button button  button--secondary"
                         onClick={() => {
-                            wishlistHasItem(item) ? removeFromWishlist(item._id) : addToWishlist(item)
+                            wishlistHasItem(item)
+                                ? removeFromWishlist(item._id)
+                                : addToWishlist(item);
                         }}
                     >
-                        {
-                            wishlistHasItem(item) ? "Unwishlist" : "Add to wishlist"
-                        }
+                        {wishlistHasItem(item)
+                            ? "Unwishlist"
+                            : "Add to wishlist"}
                     </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
