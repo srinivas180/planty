d173575b3798e97413b9fe94d0bfc8cb26c4f228
Srinivas <srinivas365@proton.me>
2023-07-24 10:26:07 +0530

    added one main toast container in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {Routes, Route} from "react-router-dom";
-import Mockman from "mockman-js"
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
 
 import "./App.css";
 import { Header } from "./components/Header/Header";
@@ -13,24 +13,27 @@ import { SingleProduct } from "./components/SingleProduct/SingleProduct";
 import { Profile } from "./pages/Profile/Profile";
 import { Checkout } from "./pages/Checkout/Checkout";
 
+import { ToastContainer } from "react-toastify";
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/product/:productId" element={<SingleProduct />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/mockman" element={<Mockman />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/product/:productId" element={<SingleProduct />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/wishlist" element={<Wishlist />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/mockman" element={<Mockman />} />
+            </Routes>
+            <ToastContainer />
+        </div>
+    );
 }
 
 export default App;
